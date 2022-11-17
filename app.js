e27e5931a157fb8a3fdd52f484f81ebc6375b087
ruineed-king <110639820+ruineed-king@users.noreply.github.com>
2022-11-17 13:20:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq )=> {
+faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+});
+});
+
+    /* ésto comprueba la localStorage si ya tiene la variable guardada */
+    function compruebaAceptaCookies() {
+        if(localStorage.aceptaCookies != 'true'){
+          cajacookies.style.display = 'block';
+        }
+      }
+      
+      /* aquí guardamos la variable de que se ha
+      aceptado el uso de cookies así no mostraremos
+      el mensaje de nuevo */
+      function aceptarCookies() {
+        localStorage.aceptaCookies = 'true';
+        cajacookies.style.display = 'none';
+      }
+      
+      /* ésto se ejecuta cuando la web está cargada */
+      $(document).ready(function () {
+        compruebaAceptaCookies();
+      });
---
+++ sr.js
@@ -0,0 +1,21 @@
+
+
+
+ScrollReveal().reveal('.contact-inv', {
+    duration:2000,
+    origin:'bottom',
+    distance:'-350px' });
+
+ScrollReveal().reveal('.container-form', {
+    duration:2000,
+    origin:'bottom',
+    distance:'-350px' });
+
+
+
+
+
+
+
+
+
